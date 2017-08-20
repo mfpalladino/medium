93351014b043d5a0b7edbc5555f7933d765b8426
Marcelo Palladino <mpalladino@directtalk.com.br>
2017-08-20 18:26:14 -0300

    Código para o artigo https://medium.com/@mfpalladino/entregando-mensagens-em-formato-raw-com-aws-sns-7e5e37da50de

---
+++ using set-subscription-attributes/src/ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.SimpleNotificationService;
+using Newtonsoft.Json;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new AmazonSimpleNotificationServiceClient(new BasicAWSCredentials("accessKey", "secretKey"), RegionEndpoint.USEast1))
+            {
+                var myPayload = new MyPayload
+                {
+                    Prop1 = "Value for property 1",
+                    Prop2 = false,
+                    Prop3 = 1000
+                };
+
+                var response = client.Publish(
+                    "arnOfSnsTopic",
+                    JsonConvert.SerializeObject(myPayload));
+            }
+        }
+    }
+
+    public class MyPayload
+    {
+        public string Prop1 { get; set; }
+        public bool Prop2 { get; set; }
+        public int Prop3 { get; set; }
+
+    }
+}
